4899ca2d93388ae16860347ef9f6b78254835937
Edwina Reynolds Baiden <rey.baiden@gmail.com>
2014-09-17 12:14:03 +0000

    second commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,88 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope) {
+	$scope.gender="";
+	$scope.dob="";
 
-}]);
+$scope.akanName=function(){
+	var mockscope = $scope;
+	if(mockscope.gender=="male" && mockscope.dob=="Monday"){
+		return "Kojo";
+
+	}else if(mockscope.gender=="female" && mockscope.dob=="Monday"){
+		return "Adjoa";
+	}
+
+	if(mockscope.gender=="male" && mockscope.dob=="Tuesday"){
+		return "Kwabena";
+
+	}else if(mockscope.gender=="female" && mockscope.dob=="Tuesday"){
+		return "Abena";
+	}
+
+	if(mockscope.gender=="male" && mockscope.dob=="Wednesday"){
+		return "Kweku";
+
+	}else if(mockscope.gender=="female" && mockscope.dob=="Wednesday"){
+		return "Akua";
+	}
+
+	if(mockscope.gender=="male" && mockscope.dob=="Thursday"){
+		return "Yaw";
+
+	}else if(mockscope.gender=="female" && mockscope.dob=="Thursday"){
+		return "Yaa";
+	}
+
+	if(mockscope.gender=="male" && mockscope.dob=="Friday"){
+		return "Kofi";
+
+	}else if(mockscope.gender=="female" && mockscope.dob=="Friday"){
+		return "Afua";
+	}
+
+	if(mockscope.gender=="male" && mockscope.dob=="Saturday"){
+		return "Kwame";
+
+	}else if(mockscope.gender=="female" && mockscope.dob=="Saturday"){
+		return "Ama";
+	}
+
+	if(mockscope.gender=="male" && mockscope.dob=="Sunday"){
+		return "Kwesi";
+
+	}else if(mockscope.gender=="female" && mockscope.dob=="Sunday"){
+		return "Akosua";
+	}
+};
+
+	
+});
+
+// <script>
+//       window.fbAsyncInit = function() {
+//         FB.init({
+//           appId      : '396744313807976',
+//           xfbml      : true,
+//           version    : 'v2.0'
+//         });
+//       };
+
+//       (function(d, s, id){
+//          var js, fjs = d.getElementsByTagName(s)[0];
+//          if (d.getElementById(id)) {return;}
+//          js = d.createElement(s); js.id = id;
+//          js.src = "//connect.facebook.net/en_US/sdk.js";
+//          fjs.parentNode.insertBefore(js, fjs);
+//        }(document, 'script', 'facebook-jssdk'));
+
+//       function testAPI() {
+//     console.log('Welcome!  Fetching your information.... ');
+//     FB.api('/me', function(response) {
+//       console.log('Successful login for: ' + response.name);
+//       document.getElementById('status').innerHTML =
+//         'Thanks for logging in, ' + response.name + '!';
+//     });
+//   }
+//     </script>
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -8,9 +8,15 @@ describe('myApp.view1 module', function() {
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
+      var mockScope={},
+       view1Ctrl = $controller('View1Ctrl', {$scope:mockScope});
       expect(view1Ctrl).toBeDefined();
     }));
 
+    it('should display the akanname in the scope', inject(function($controller){
+    	var mockScope={},
+    	akanNameCtrl= $controller('View1Ctrl', {$scope:mockScope});
+    	expect(mockScope.akanName).toBeDefined();
+    }))
   });
 });
